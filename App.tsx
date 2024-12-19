eedc69d68fa4725274226f08c0704e2123854a4d
ZzzandyzzZ <andy_gean@hotmail.com>
2024-12-19 10:56:14 -0500

    Setup typescript

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+interface Props {
+
+}
 export default function App() {
+  const a:string = 'asdasd'
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
+      <Text>Hello World asdas</Text>
+      <Text>{a}</Text>
       <StatusBar style="auto" />
     </View>
   );
